94621c305fc9ef12537df026b577f18811d6863f
Don Schemmel <dons500@gmail.com>
2015-09-14 13:13:53 -0700

    Add JS to display place fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
 
+    var placeName = $("input#new-place-name").val();
+    var attraction = $("input#new-attraction").val();
+    var city = $("input#new-city").val();
+    var state = $("input#new-state").val();
+    var newPlace = { placeName: placeName, attraction: attraction, city: city,
+      state: state };
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.placeName + " " + newPlace.attraction +
+    " " + newPlace.city + " " + newPlace.state + "</span></li>");
+
+    $("input#new-place-name").val("");
+    $("input#new-attraction").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+  });
+});
